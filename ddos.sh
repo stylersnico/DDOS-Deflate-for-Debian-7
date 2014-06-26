aa4911108f2e0d1185f8e959f7e50412f41849ea
Nicolas Simond <stylersnico@gmail.com>
2014-06-26 16:56:43 +0200

    Update ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -49,25 +49,25 @@ unbanip()
 	UNBAN_SCRIPT=`mktemp /tmp/unban.XXXXXXXX`
 	TMP_FILE=`mktemp /tmp/unban.XXXXXXXX`
 	UNBAN_IP_LIST=`mktemp /tmp/unban.XXXXXXXX`
-	echo '#!/bin/bash' > "$UNBAN_SCRIPT"
-	echo "sleep $BAN_PERIOD" >> "$UNBAN_SCRIPT"
-	if [ "$APF_BAN" -eq 1 ]; then
+	echo '#!/bin/sh' > $UNBAN_SCRIPT
+	echo "sleep $BAN_PERIOD" >> $UNBAN_SCRIPT
+	if [ $APF_BAN -eq 1 ]; then
 		while read line; do
-			echo "$APF -u $line" >> "$UNBAN_SCRIPT"
-			echo "$line" >> "$UNBAN_IP_LIST"
-		done < "$BANNED_IP_LIST"
+			echo "$APF -u $line" >> $UNBAN_SCRIPT
+			echo $line >> $UNBAN_IP_LIST
+		done < $BANNED_IP_LIST
 	else
 		while read line; do
-			echo "$IPT -D INPUT -s $line -j DROP" >> "$UNBAN_SCRIPT"
-			echo $line >> "$UNBAN_IP_LIST"
-		done < "$BANNED_IP_LIST"
+			echo "$IPT -D INPUT -s $line -j DROP" >> $UNBAN_SCRIPT
+			echo $line >> $UNBAN_IP_LIST
+		done < $BANNED_IP_LIST
 	fi
-	echo "grep -v --file=$UNBAN_IP_LIST $IGNORE_IP_LIST > $TMP_FILE" >> "$UNBAN_SCRIPT"
-	echo "mv $TMP_FILE $IGNORE_IP_LIST" >> "$UNBAN_SCRIPT"
-	echo "rm -f $UNBAN_SCRIPT" >> "$UNBAN_SCRIPT"
-	echo "rm -f $UNBAN_IP_LIST" >> "$UNBAN_SCRIPT"
-	echo "rm -f $TMP_FILE" >> "$UNBAN_SCRIPT"
-	. "$UNBAN_SCRIPT" &
+	echo "grep -v --file=$UNBAN_IP_LIST $IGNORE_IP_LIST > $TMP_FILE" >> $UNBAN_SCRIPT
+	echo "mv $TMP_FILE $IGNORE_IP_LIST" >> $UNBAN_SCRIPT
+	echo "rm -f $UNBAN_SCRIPT" >> $UNBAN_SCRIPT
+	echo "rm -f $UNBAN_IP_LIST" >> $UNBAN_SCRIPT
+	echo "rm -f $TMP_FILE" >> $UNBAN_SCRIPT
+	. $UNBAN_SCRIPT &
 }
 
 add_to_cron()
@@ -82,8 +82,8 @@ add_to_cron()
 
 
 load_conf
-while [ "$1" ]; do
-	case "$1" in
+while [ $1 ]; do
+	case $1 in
 		'-h' | '--help' | '?' )
 			showhelp
 			exit
@@ -110,37 +110,37 @@ TMP_PREFIX='/tmp/ddos'
 TMP_FILE="mktemp $TMP_PREFIX.XXXXXXXX"
 BANNED_IP_MAIL=`$TMP_FILE`
 BANNED_IP_LIST=`$TMP_FILE`
-echo "Banned the following ip addresses on date" > "$BANNED_IP_MAIL"
-echo >>	"$BANNED_IP_MAIL"
+echo "Banned the following ip addresses on `date`" > $BANNED_IP_MAIL
+echo >>	$BANNED_IP_MAIL
 BAD_IP_LIST=`$TMP_FILE`
-netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr > "$BAD_IP_LIST"
-cat "$BAD_IP_LIST"
+netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr > $BAD_IP_LIST
+cat $BAD_IP_LIST
 if [ $KILL -eq 1 ]; then
 	IP_BAN_NOW=0
 	while read line; do
-		CURR_LINE_CONN=$(echo "$line" | cut -d" " -f1)
-		CURR_LINE_IP=$(echo "$line" | cut -d" " -f2)
-		if [ "$CURR_LINE_CONN" -lt "$NO_OF_CONNECTIONS" ]; then
+		CURR_LINE_CONN=$(echo $line | cut -d" " -f1)
+		CURR_LINE_IP=$(echo $line | cut -d" " -f2)
+		if [ $CURR_LINE_CONN -lt $NO_OF_CONNECTIONS ]; then
 			break
 		fi
-		IGNORE_BAN="grep -c $CURR_LINE_IP $IGNORE_IP_LIST"
-		if [ "$IGNORE_BAN" -ge 1 ]; then
+		IGNORE_BAN=`grep -c $CURR_LINE_IP $IGNORE_IP_LIST`
+		if [ $IGNORE_BAN -ge 1 ]; then
 			continue
 		fi
 		IP_BAN_NOW=1
-		echo "$CURR_LINE_IP with $CURR_LINE_CONN connections" >> "$BANNED_IP_MAIL"
-		echo "$CURR_LINE_IP" >> "$BANNED_IP_LIST"
-		echo "$CURR_LINE_IP" >> "$IGNORE_IP_LIST"
-		if [ "$APF_BAN" -eq 1 ]; then
-			"$APF" -d "$CURR_LINE_IP"
+		echo "$CURR_LINE_IP with $CURR_LINE_CONN connections" >> $BANNED_IP_MAIL
+		echo $CURR_LINE_IP >> $BANNED_IP_LIST
+		echo $CURR_LINE_IP >> $IGNORE_IP_LIST
+		if [ $APF_BAN -eq 1 ]; then
+			$APF -d $CURR_LINE_IP
 		else
-			"$IPT" -I INPUT -s "$CURR_LINE_IP" -j DROP
+			$IPT -I INPUT -s $CURR_LINE_IP -j DROP
 		fi
-	done < "$BAD_IP_LIST"
-	if [ "$IP_BAN_NOW" -eq 1 ]; then
-		dt="date"
-		if [ "$EMAIL_TO" != "" ]; then
-			cat "$BANNED_IP_MAIL" | mail -s "IP addresses banned on $dt" "$EMAIL_TO"
+	done < $BAD_IP_LIST
+	if [ $IP_BAN_NOW -eq 1 ]; then
+		dt=`date`
+		if [ $EMAIL_TO != "" ]; then
+			cat $BANNED_IP_MAIL | mail -s "IP addresses banned on $dt" $EMAIL_TO
 		fi
 		unbanip
 	fi
